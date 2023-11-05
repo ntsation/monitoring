dab9afd637ff4c65a018a35d33d0ee2809d1e0a6
Nathan Souza <naathanmarinho11@gmail.com>
2023-11-05 10:21:56 -0300

    add shell code for monitoring

---
+++ monitoring.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Coletar informações de recursos
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1)
+mem_usage=$(free | grep Mem | awk '{print $3/$2 * 100}')
+disk_usage=$(df -h | grep '/dev/sda1' | awk '{print $5}' | cut -d% -f1)
+
+echo "CPU_USAGE=$cpu_usage"
+echo "MEM_USAGE=$mem_usage"
+echo "DISK_USAGE=$disk_usage"
