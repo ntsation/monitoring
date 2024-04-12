81897af5934705bb413d3b029f5692e6f70761cf
Nathan Souza <naathanmarinho11@gmail.com>
2024-04-11 23:24:38 -0300

    atualiza sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Collect resource information
+# Coleta as informações
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2}' | cut -d. -f1)
 mem_usage=$(free | grep Mem | awk '{print $3/$2 * 100}')
 disk_usage=$(df -h | grep '/dev/sda1' | awk '{print $5}' | cut -d% -f1)
